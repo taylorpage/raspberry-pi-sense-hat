52990d582b0377ee8bed58dd4e3d04e415213c42
Taylor Page <jonathantaylorpage@gmail.com>
2018-06-24 22:44:30 -0700

    Create majoras mask slide effect
    - Create 42 frame animation for panning majoras mask

---
+++ grids/majoras_mask.py
@@ -0,0 +1,470 @@
+l = (0,0,0)
+b = (0,0,255)
+r = (255,0,0)
+y = (255,255,0)
+o = (255,100,0)
+p = (255,0,255)
+t = (0,255,255)
+e = (0,0,0)
+
+frame1 = [
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+]
+
+frame2 = [
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,l,
+    e,e,e,e,e,e,e,l,
+    e,e,e,e,e,e,e,e,
+]
+
+frame3 = [
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,l,
+    e,e,e,e,e,e,l,y,
+    e,e,e,e,e,e,l,l,
+    e,e,e,e,e,e,e,e,
+]
+
+frame4 = [
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,l,
+    e,e,e,e,e,e,l,y,
+    e,e,e,e,e,l,y,o,
+    e,e,e,e,e,l,l,l,
+    e,e,e,e,e,e,e,e,
+]
+
+frame5 = [
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,l,
+    e,e,e,e,e,e,l,y,
+    e,e,e,e,e,l,y,o,
+    e,e,e,e,l,y,o,o,
+    e,e,e,e,l,l,l,l,
+    e,e,e,e,e,e,e,e,
+]
+
+frame6 = [
+    e,e,e,e,e,e,e,l,
+    e,e,e,e,e,e,e,l,
+    e,e,e,e,e,e,l,y,
+    e,e,e,e,e,l,y,o,
+    e,e,e,e,l,y,o,o,
+    e,e,e,l,y,o,o,l,
+    e,e,e,l,l,l,l,e,
+    e,e,e,e,e,e,e,l,
+]
+
+frame7 = [
+    e,e,e,e,e,e,l,o,
+    e,e,e,e,e,e,l,b,
+    e,e,e,e,e,l,y,l,
+    e,e,e,e,l,y,o,l,
+    e,e,e,l,y,o,o,o,
+    e,e,l,y,o,o,l,l,
+    e,e,l,l,l,l,e,l,
+    e,e,e,e,e,e,l,b,
+]
+
+frame8 = [
+    e,e,e,e,e,l,o,b,
+    e,e,e,e,e,l,b,b,
+    e,e,e,e,l,y,l,b,
+    e,e,e,l,y,o,l,b,
+    e,e,l,y,o,o,o,l,
+    e,l,y,o,o,l,l,l,
+    e,l,l,l,l,e,l,b,
+    e,e,e,e,e,l,b,r,
+]
+
+frame9 = [
+    e,e,e,e,l,o,b,r,
+    e,e,e,e,l,b,b,r,
+    e,e,e,l,y,l,b,r,
+    e,e,l,y,o,l,b,r,
+    e,l,y,o,o,o,l,r,
+    l,y,o,o,l,l,l,l,
+    l,l,l,l,e,l,b,r,
+    e,e,e,e,l,b,r,r,
+]
+
+frame10 = [
+    e,e,e,l,o,b,r,l,
+    e,e,e,l,b,b,r,l,
+    e,e,l,y,l,b,r,l,
+    e,l,y,o,l,b,r,l,
+    l,y,o,o,o,l,r,l,
+    y,o,o,l,l,l,l,r,
+    l,l,l,e,l,b,r,l,
+    e,e,e,l,b,r,r,r,
+]
+
+frame11 = [
+    e,e,l,o,b,r,l,l,
+    e,e,l,b,b,r,l,l,
+    e,l,y,l,b,r,l,o,
+    l,y,o,l,b,r,l,o,
+    y,o,o,o,l,r,l,o,
+    o,o,l,l,l,l,r,l,
+    l,l,e,l,b,r,l,r,
+    e,e,l,b,r,r,r,l,
+]
+
+frame12 = [
+    e,l,o,b,r,l,l,l,
+    e,l,b,b,r,l,l,o,
+    l,y,l,b,r,l,o,y,
+    y,o,l,b,r,l,o,t,
+    o,o,o,l,r,l,o,y,
+    o,l,l,l,l,r,l,o,
+    l,e,l,b,r,l,r,l,
+    e,l,b,r,r,r,l,r,
+]
+
+frame13 = [
+    l,o,b,r,l,l,l,l,
+    l,b,b,r,l,l,o,o,
+    y,l,b,r,l,o,y,t,
+    o,l,b,r,l,o,t,l,
+    o,o,l,r,l,o,y,t,
+    l,l,l,l,r,l,o,o,
+    e,l,b,r,l,r,l,l,
+    l,b,r,r,r,l,r,r,
+]
+
+frame14 = [
+    o,b,r,l,l,l,l,l,
+    b,b,r,l,l,o,o,o,
+    l,b,r,l,o,y,t,y,
+    l,b,r,l,o,t,l,t,
+    o,l,r,l,o,y,t,y,
+    l,l,l,r,l,o,o,o,
+    l,b,r,l,r,l,l,l,
+    b,r,r,r,l,r,r,r,
+]
+
+frame15 = [
+    b,r,l,l,l,l,l,r,
+    b,r,l,l,o,o,o,l,
+    b,r,l,o,y,t,y,o,
+    b,r,l,o,t,l,t,o,
+    l,r,l,o,y,t,y,o,
+    l,l,r,l,o,o,o,l,
+    b,r,l,r,l,l,l,l,
+    r,r,r,l,r,r,r,r,
+]
+
+frame16 = [
+    r,l,l,l,l,l,r,b,
+    r,l,l,o,o,o,l,r,
+    r,l,o,y,t,y,o,l,
+    r,l,o,t,l,t,o,l,
+    r,l,o,y,t,y,o,l,
+    l,r,l,o,o,o,l,l,
+    r,l,r,l,l,l,l,l,
+    r,r,l,r,r,r,r,r,
+]
+
+frame17 = [
+    l,l,l,l,l,r,b,t,
+    l,l,o,o,o,l,r,b,
+    l,o,y,t,y,o,l,r,
+    l,o,t,l,t,o,l,r,
+    l,o,y,t,y,o,l,r,
+    r,l,o,o,o,l,l,r,
+    l,r,l,l,l,l,l,r,
+    r,l,r,r,r,r,r,r,
+]
+
+frame18 = [
+    l,l,l,l,r,b,t,b,
+    l,o,o,o,l,r,b,t,
+    o,y,t,y,o,l,r,b,
+    o,t,l,t,o,l,r,b,
+    o,y,t,y,o,l,r,b,
+    l,o,o,o,l,l,r,b,
+    r,l,l,l,l,l,r,r,
+    l,r,r,r,r,r,r,r,
+]
+
+frame19 = [
+    l,l,l,r,b,t,b,t,
+    o,o,o,l,r,b,t,b,
+    y,t,y,o,l,r,b,r,
+    t,l,t,o,l,r,b,r,
+    y,t,y,o,l,r,b,r,
+    o,o,o,l,l,r,b,r,
+    l,l,l,l,l,r,r,r,
+    r,r,r,r,r,r,r,r,
+]
+
+frame20 = [
+    l,l,r,b,t,b,t,b,
+    o,o,l,r,b,t,b,r,
+    t,y,o,l,r,b,r,l,
+    l,t,o,l,r,b,r,l,
+    t,y,o,l,r,b,r,l,
+    o,o,l,l,r,b,r,l,
+    l,l,l,l,r,r,r,l,
+    r,r,r,r,r,r,r,r,
+]
+
+frame21 = [
+    l,r,b,t,b,t,b,r,
+    o,l,r,b,t,b,r,l,
+    y,o,l,r,b,r,l,o,
+    t,o,l,r,b,r,l,o,
+    y,o,l,r,b,r,l,o,
+    o,l,l,r,b,r,l,l,
+    l,l,l,r,r,r,l,l,
+    r,r,r,r,r,r,r,r,
+]
+
+frame22 = [
+    r,b,t,b,t,b,r,l,
+    l,r,b,t,b,r,l,o,
+    o,l,r,b,r,l,o,y,
+    o,l,r,b,r,l,o,t,
+    o,l,r,b,r,l,o,y,
+    l,l,r,b,r,l,l,o,
+    l,l,r,r,r,l,l,l,
+    r,r,r,r,r,r,r,r,
+]
+
+frame23 = [
+    b,t,b,t,b,r,l,l,
+    r,b,t,b,r,l,o,o,
+    l,r,b,r,l,o,y,t,
+    l,r,b,r,l,o,t,l,
+    l,r,b,r,l,o,y,t,
+    l,r,b,r,l,l,o,o,
+    l,r,r,r,l,l,l,l,
+    r,r,r,r,r,r,r,r,
+]
+
+frame24 = [
+    t,b,t,b,r,l,l,l,
+    b,t,b,r,l,o,o,o,
+    r,b,r,l,o,y,t,y,
+    r,b,r,l,o,t,l,t,
+    r,b,r,l,o,y,t,y,
+    r,b,r,l,l,o,o,o,
+    r,r,r,l,l,l,l,l,
+    r,r,r,r,r,r,r,r,
+]
+
+frame25 = [
+    b,t,b,r,l,l,l,l,
+    t,b,r,l,o,o,o,l,
+    b,r,l,o,y,t,y,o,
+    b,r,l,o,t,l,t,o,
+    b,r,l,o,y,t,y,o,
+    b,r,l,l,o,o,o,l,
+    r,r,l,l,l,l,l,r,
+    r,r,r,r,r,r,r,l,
+]
+
+frame26 = [
+    t,b,r,l,l,l,l,l,
+    b,r,l,o,o,o,l,l,
+    r,l,o,y,t,y,o,l,
+    r,l,o,t,l,t,o,l,
+    r,l,o,y,t,y,o,l,
+    r,l,l,o,o,o,l,r,
+    r,l,l,l,l,l,r,l,
+    r,r,r,r,r,r,l,r,
+]
+
+frame27 = [
+    b,r,l,l,l,l,l,r,
+    r,l,o,o,o,l,l,r,
+    l,o,y,t,y,o,l,r,
+    l,o,t,l,t,o,l,r,
+    l,o,y,t,y,o,l,r,
+    l,l,o,o,o,l,r,l,
+    l,l,l,l,l,r,l,r,
+    r,r,r,r,r,l,r,r,
+]
+
+frame28 = [
+    r,l,l,l,l,l,r,b,
+    l,o,o,o,l,l,r,b,
+    o,y,t,y,o,l,r,b,
+    o,t,l,t,o,l,r,b,
+    o,y,t,y,o,l,r,l,
+    l,o,o,o,l,r,l,l,
+    l,l,l,l,r,l,r,b,
+    r,r,r,r,l,r,r,r,
+]
+
+frame29 = [
+    l,l,l,l,l,r,b,o,
+    o,o,o,l,l,r,b,b,
+    y,t,y,o,l,r,b,l,
+    t,l,t,o,l,r,b,l,
+    y,t,y,o,l,r,l,o,
+    o,o,o,l,r,l,l,l,
+    l,l,l,r,l,r,b,l,
+    r,r,r,l,r,r,r,b,
+]
+
+frame30 = [
+    l,l,l,l,r,b,o,l,
+    o,o,l,l,r,b,b,l,
+    t,y,o,l,r,b,l,y,
+    l,t,o,l,r,b,l,o,
+    t,y,o,l,r,l,o,o,
+    o,o,l,r,l,l,l,l,
+    l,l,r,l,r,b,l,e,
+    r,r,l,r,r,r,b,l,
+]
+
+frame31 = [
+    l,l,l,r,b,o,l,e,
+    o,l,l,r,b,b,l,e,
+    y,o,l,r,b,l,y,l,
+    t,o,l,r,b,l,o,y,
+    y,o,l,r,l,o,o,o,
+    o,l,r,l,l,l,l,o,
+    l,r,l,r,b,l,e,l,
+    r,l,r,r,r,b,l,e,
+]
+
+frame32 = [
+    l,l,r,b,o,l,e,e,
+    l,l,r,b,b,l,e,e,
+    o,l,r,b,l,y,l,e,
+    o,l,r,b,l,o,y,l,
+    o,l,r,l,o,o,o,y,
+    l,r,l,l,l,l,o,o,
+    r,l,r,b,l,e,l,l,
+    l,r,r,r,b,l,e,e,
+]
+
+frame33 = [
+    l,r,b,o,l,e,e,e,
+    l,r,b,b,l,e,e,e,
+    l,r,b,l,y,l,e,e,
+    l,r,b,l,o,y,l,e,
+    l,r,l,o,o,o,y,l,
+    r,l,l,l,l,o,o,y,
+    l,r,b,l,e,l,l,l,
+    r,r,r,b,l,e,e,e,
+]
+
+frame34 = [
+    r,b,o,l,e,e,e,e,
+    r,b,b,l,e,e,e,e,
+    r,b,l,y,l,e,e,e,
+    r,b,l,o,y,l,e,e,
+    r,l,o,o,o,y,l,e,
+    l,l,l,l,o,o,y,l,
+    r,b,l,e,l,l,l,l,
+    r,r,b,l,e,e,e,e,
+]
+
+frame35 = [
+    b,o,l,e,e,e,e,e,
+    b,b,l,e,e,e,e,e,
+    b,l,y,l,e,e,e,e,
+    b,l,o,y,l,e,e,e,
+    l,o,o,o,y,l,e,e,
+    l,l,l,o,o,y,l,e,
+    b,l,e,l,l,l,l,e,
+    r,b,l,e,e,e,e,e,
+]
+
+frame36 = [
+    o,l,e,e,e,e,e,e,
+    b,l,e,e,e,e,e,e,
+    l,y,l,e,e,e,e,e,
+    l,o,y,l,e,e,e,e,
+    o,o,o,y,l,e,e,e,
+    l,l,o,o,y,l,e,e,
+    l,e,l,l,l,l,e,e,
+    b,l,e,e,e,e,e,e,
+]
+
+frame37 = [
+    l,e,e,e,e,e,e,e,
+    l,e,e,e,e,e,e,e,
+    y,l,e,e,e,e,e,e,
+    o,y,l,e,e,e,e,e,
+    o,o,y,l,e,e,e,e,
+    l,o,o,y,l,e,e,e,
+    e,l,l,l,l,e,e,e,
+    l,e,e,e,e,e,e,e,
+]
+
+frame38 = [
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    l,e,e,e,e,e,e,e,
+    y,l,e,e,e,e,e,e,
+    o,y,l,e,e,e,e,e,
+    o,o,y,l,e,e,e,e,
+    l,l,l,l,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+]
+
+frame39 = [
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    l,e,e,e,e,e,e,e,
+    y,l,e,e,e,e,e,e,
+    o,y,l,e,e,e,e,e,
+    l,l,l,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+]
+
+frame40 = [
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    l,e,e,e,e,e,e,e,
+    y,l,e,e,e,e,e,e,
+    l,l,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+]
+
+frame41 = [
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    l,e,e,e,e,e,e,e,
+    l,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+]
+
+frame42 = [
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+    e,e,e,e,e,e,e,e,
+]
